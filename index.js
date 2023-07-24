9785905d2ec7661487abf8491152528ff03a97b7
jennipher <voliduser@gmail.com>
2023-07-24 19:08:59 -0400

    domlab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const newHeader = document.createElement('h1')
+newHeader.id='victory'
+newHeader.innerHTML="YOUR-NAME is the champion"
+/*
+<h1></h1>
+<h1 id="victory"></h1>
+<h1 id="victory">YOUR-NAME is the champion</h1>
+*/
--- test/indexTest.js
+++ test/indexTest.js
@@ -3,7 +3,12 @@ require ( './helpers.js' );
 describe("index.html", () => {
   describe("after index.js is processed", () => {
     it("no longer has DOM node 'main#main'", () => {
-      expect(document.querySelector('main#main'), "Make sure you remove the <main> with id 'main'").to.not.exist
+      expect(document.querySelector('main'), "Make sure you remove the <main> with id 'main'").to.not.exist
+    });
+
+    it("has a 'newHeader' variable that points to node 'h1#victory'", () => {
+      console.log(newHeader)
+      expect(newHeader.nodeName, "Make sure you create an <h1> with id 'victory'").eql('H1')
     });
 
     it("has a 'newHeader' variable that points to an <h1> node", () => {
